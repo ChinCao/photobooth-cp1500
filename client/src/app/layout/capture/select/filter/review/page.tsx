badf3012e0cc47fab5a267f903a648c7124e474b
Cao Cự Chính <chinhcaocu@gmail.com>
2025-02-18 12:19:13 +0700

    Update review page with countdown timer and added heart animation

--- client/src/app/layout/capture/select/filter/review/page.tsx
+++ client/src/app/layout/capture/select/filter/review/page.tsx
@@ -7,7 +7,7 @@ import {useTranslation} from "react-i18next";
 const ReviewPage = () => {
   const {photo, setPhoto} = usePhoto();
   const router = useRouter();
-  const [timeLeft, setTimeLeft] = useState(7);
+  const [timeLeft, setTimeLeft] = useState(6);
   const {t} = useTranslation();
   useEffect(() => {
     if (!photo) return router.push("/");
@@ -32,7 +32,7 @@ const ReviewPage = () => {
   return (
     <div className="w-full h-full flex items-center justify-center gap-6 flex-col">
       {photo && (
-        <div className="flex items-center justify-center gap-4">
+        <div className="flex items-center justify-around gap-4">
           <video
             className="w-[70%] rounded-lg"
             src={photo.video.r2_url!}
@@ -47,13 +47,22 @@ const ReviewPage = () => {
           <div className="flex flex-col gap-5 items-center justify-center">
             <h1 className="text-4xl font-bold text-center">{t("Please go outside to take the photo")}</h1>
             <p className="text-4xl font-bold text-center text-red-500">💫 {timeLeft}s</p>
-            <Image
-              src="/dance.gif"
-              alt="logo"
-              unoptimized
-              width={190}
-              height={190}
-            />
+            <div className="flex items-center justify-center">
+              <Image
+                src="/dance.gif"
+                alt="logo"
+                unoptimized
+                width={160}
+                height={160}
+              />
+              <Image
+                src="/heart.gif"
+                alt="logo"
+                unoptimized
+                width={160}
+                height={160}
+              />
+            </div>
           </div>
         </div>
       )}
